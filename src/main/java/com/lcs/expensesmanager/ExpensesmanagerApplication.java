f64de94452a41e691bae0788386b3bd9e93b54bb
Lucas Martins <lucas.mrt.dev@gmail.com>
2024-05-25 22:28:28 -0300

    project added to repo

---
+++ src/main/java/com/lcs/expensesmanager/ExpensesmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.lcs.expensesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lcs/expensesmanager/ExpensesmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcs.expensesmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
